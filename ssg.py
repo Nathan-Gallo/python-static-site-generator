0faae279ed2ea0c2e8f2767c6ad56db020bbe753
Gallo, Nathan M (HIMCO, FIN) <nathan.gallo@thehartford.com>
2020-11-30 15:43:49 -0500

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -1,14 +1,13 @@
 import typer
-import Site from ssg.Site
+from ssg.site import Site
 
 
-def main(source = 'content', dest = 'dist'):
+def main(source='content', dest='dist'):
     config = {
         "source": source,
         "dest": dest
     }
+    Site(**config).build()
 
-    site = Site(**config)
-    site.build()
 
 typer.run(main)
