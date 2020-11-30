58460d07a4797bb519fb8a69607c513bbf18a393
Gallo, Nathan M (HIMCO, FIN) <nathan.gallo@thehartford.com>
2020-11-30 15:44:58 -0500

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 class Site:
     def __init__(self, source, dest):
         self.source = Path(source)
@@ -10,9 +11,7 @@ class Site:
         directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
-        self.dest.mkdir(parents=True, exist_ok=True) 
+        self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
-                self.create_dir(path) 
-
-    
+                self.create_dir(path)
