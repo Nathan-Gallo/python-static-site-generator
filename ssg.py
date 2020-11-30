29a65c4c8f4270d506df1366f2479dd61b8fd12b
Gallo, Nathan M (HIMCO, FIN) <nathan.gallo@thehartford.com>
2020-11-30 15:28:38 -0500

    module 1

---
+++ ssg.py
@@ -0,0 +1,14 @@
+import typer
+import Site from ssg.Site
+
+
+def main(source = 'content', dest = 'dist'):
+    config = {
+        "source": source,
+        "dest": dest
+    }
+
+    site = Site(**config)
+    site.build()
+
+typer.run(main)
